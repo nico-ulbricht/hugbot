be1e9bf5b0daab3091d752563af7a5c73425a7b7
Nico Ulbricht <nico@Nicos-MacBook-Air.local>
2019-11-16 15:24:23 +0100

    add panic implementations to reaction service & repository

--- pkg/reaction/repository.go
+++ pkg/reaction/repository.go
@@ -11,3 +11,22 @@ type Repository interface {
 	GetByRecipientID(ctx context.Context, recipientID uuid.UUID) ([]*Reaction, error)
 	GetBySenderID(ctx context.Context, senderID uuid.UUID) ([]*Reaction, error)
 }
+
+type repository struct {}
+
+
+func (rp *repository)	Insert(ctx context.Context, reaction *Reaction) (*Reaction, error) {
+	panic("TODO")
+}
+
+func (rp *repository)	GetByRecipientID(ctx context.Context, recipientID uuid.UUID) ([]*Reaction, error) {
+	panic("TODO")
+}
+
+func (rp *repository)	GetBySenderID(ctx context.Context, senderID uuid.UUID) ([]*Reaction, error) {
+	panic("TODO")
+}
+
+func NewRepository() {
+	return &repository{}
+}
